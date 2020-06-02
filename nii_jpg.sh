d8f62ae720d107131dfe9bc791f3ac2b03c908d3
Daryl Fung <darylfung96@gmail.com>
2020-06-01 22:39:13 -0500

    added multiclass segment

--- nii_jpg.sh
+++ nii_jpg.sh
@@ -17,6 +17,16 @@ python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/7.nii --filename_prefix rp
 python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/8.nii --filename_prefix rp_8 --output_folder Inf-Net/Dataset/TrainingSet/LungInfection-Train/Imgs/ --save_type jpg
 python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/9.nii --filename_prefix rp_9 --output_folder Inf-Net/Dataset/TrainingSet/LungInfection-Train/Imgs/ --save_type jpg
 
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/1.nii --filename_prefix rp_1 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/2.nii --filename_prefix rp_2 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/3.nii --filename_prefix rp_3 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/4.nii --filename_prefix rp_4 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/5.nii --filename_prefix rp_5 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/6.nii --filename_prefix rp_6 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/7.nii --filename_prefix rp_7 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/8.nii --filename_prefix rp_8 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_im/9.nii --filename_prefix rp_9 --output_folder Inf-Net/Dataset/TrainingSet/MultiClassInfection-Train/Imgs/ --save_type jpg
+
 
 //rp_1_mask
 python nii_to_jpg.py --nii_file Inf-Net/Dataset/rp_msk/1.nii --filename_prefix rp_1 --output_folder Inf-Net/Dataset/TrainingSet/LungInfection-Train/GT/ --save_type png --is_binary True
