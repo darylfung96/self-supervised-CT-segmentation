9f767729799d8d5565bce5dd274dea50ec2e7873
Daryl Fung <darylfung96@gmail.com>
2020-06-06 11:52:04 -0500

    remove invalid arg

--- Inf-Net/Code/utils/onehot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def onehot(data, n):
-    """onehot ecoder"""
-    buf = np.zeros(data.shape + (n,))
-    nmsk = np.arange(data.size) * n + data.ravel()
-    buf.ravel()[nmsk - 1] = 1
-    return buf
