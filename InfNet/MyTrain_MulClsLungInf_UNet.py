f76b3edd1e86645e32cb8be9252ae379e6fade9f
Daryl Fung <darylfung96@gmail.com>
2020-06-12 09:36:06 -0500

    fix testing error for multi

--- InfNet/MyTrain_MulClsLungInf_UNet.py
+++ InfNet/MyTrain_MulClsLungInf_UNet.py
@@ -38,7 +38,7 @@ def train(epo_num, num_classes, input_channels, batch_size, lr, graph_path, save
         transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]),
-        is_test=True
+        is_test=False
     )
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
 
