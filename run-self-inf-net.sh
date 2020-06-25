07dc1a415a4830e16e6110c9b9a06a8c8cb4dc44
Daryl Fung <darylfung96@gmail.com>
2020-06-25 10:39:17 -0500

    added scripts

--- run-self-inf-net.sh
+++ run-self-inf-net.sh
@@ -1,2 +1,2 @@
- python main_inf-net.py --save_path model/self-inf-net --graph_path graphs/self-inf-net --device cuda --seed 7
+python main_inf-net.py --save_path model/self-inf-net --graph_path graphs/self-inf-net --device cuda --seed 7
 python MyTrain_LungInf.py --train_save self-improved --is_data_augment True --graph_path graph_self-improved --load_net_path ../model/self-inf-net/self-inf-netmedseg_resnet18_autoencoder_no_bottleneck_use_coach0.net.best.ckpt.t7 --device cuda --epoch 500 --batchsize 12
