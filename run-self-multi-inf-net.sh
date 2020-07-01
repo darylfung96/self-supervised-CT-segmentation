d7d7d9ef9323686fb818606ab78043121d76b3ba
Daryl Fung <darylfung96@gmail.com>
2020-06-30 21:47:10 -0500

    fix coach error

--- run-self-multi-inf-net.sh
+++ run-self-multi-inf-net.sh
@@ -1,1 +1,2 @@
-python main_multi-inf-net.py --save_path model/self_multi_improved --graph_path graph_self_multi_improved --device cuda --seed 7 --batchsize 12 --is_data_augment True --is_label_smooth True --random_cutout 0.4
+# check again maybe batchsize can be changed
+python main_multi-inf-net.py --save_path model/self_multi_improved --graph_path graph_self_multi_improved --device cuda --seed 7
