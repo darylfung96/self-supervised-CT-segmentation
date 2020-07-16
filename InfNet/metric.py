2873bd78910c4739c594a6389ad08c88d0d0b867
Daryl Fung <darylfung96@gmail.com>
2020-07-16 09:29:27 -0500

    fix metrics error with cuda

--- InfNet/metric.py
+++ InfNet/metric.py
@@ -41,8 +41,8 @@ def sensitivity_similarity_coefficient(predicted_seg, ground_truth_seg):
     a = predicted_seg.view(-1)
     b = ground_truth_seg.view(-1)
 
-    true_positive = (a * b).sum().detach().numpy()  # because ground truth is 1, and remove all the other false positive
-    false_negative = (b - a).detach().numpy()
+    true_positive = (a * b).sum().detach().cpu().numpy()  # because ground truth is 1, and remove all the other false positive
+    false_negative = (b - a).detach().cpu().numpy()
     false_negative[false_negative < 0] = 0
     false_negative = false_negative.sum()
     sensitivity = true_positive/(false_negative + true_positive)
@@ -56,8 +56,8 @@ def specificity_similarity_coefficient(predicted_seg, ground_truth_seg):
     inverted_b = 1 - b
     inverted_a = 1 - a
 
-    true_negative = (inverted_a * inverted_b).sum().detach().numpy()
-    false_positive = (inverted_b - inverted_a).detach().numpy()
+    true_negative = (inverted_a * inverted_b).sum().detach().cpu().numpy()
+    false_positive = (inverted_b - inverted_a).detach().cpu().numpy()
     false_positive[false_positive < 0] = 0
     false_positive = false_positive.sum()
     specificity = true_negative / (false_positive + true_negative)
