abd66d2f80a0a0e8c1f0096afb19b16262745c1d
Daryl Fung <darylfung96@gmail.com>
2020-08-06 15:31:35 -0500

    added roc networks

--- InfNet/Code/model_lung_infection/InfNet_UNet.py
+++ InfNet/Code/model_lung_infection/InfNet_UNet.py
@@ -61,3 +61,63 @@ class Inf_Net_UNet(nn.Module):
         logits = self.outc(x)
 
         return logits
+
+
+class Inf_Net_UNet_Improved(nn.Module):
+    """ Full assembly of the parts to form the complete network """
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(Inf_Net_UNet_Improved, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(262, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+
+        self.up1 = Up(1030, 512 // factor, bilinear)
+        self.up2 = Up(518, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+        # inpainting weights
+        self.up1_inpainting = Up(1030, 512 // factor, bilinear)
+        self.up2_inpainting = Up(518, 256 // factor, bilinear)
+        self.up3_inpainting = Up(256, 128 // factor, bilinear)
+        self.up4_inpainting = Up(128, 64, bilinear)
+        self.out_inpainting = OutConv(64, n_channels)
+
+    def forward_inpainting(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x3 = torch.cat((x3, F.interpolate(x, size=32)), 1)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x5 = torch.cat((x5, F.interpolate(x, size=8)), dim=1)
+        x_out = self.up1_inpainting(x5, x4)
+        x_out = self.up2_inpainting(x_out, x3)
+        x_out = self.up3_inpainting(x_out, x2)
+        x_out = self.up4_inpainting(x_out, x1)
+        reconstructed_image = F.tanh(self.out_inpainting(x_out))
+
+        return reconstructed_image
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x3 = torch.cat((x3, F.interpolate(x, size=32)), dim=1)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x5 = torch.cat((x5, F.interpolate(x, size=8)), dim=1)
+        x_out = self.up1(x5, x4)
+        x_out = self.up2(x_out, x3)
+        x_out = self.up3(x_out, x2)
+        x_out = self.up4(x_out, x1)
+        logits = self.outc(x_out)
+        return logits
---
+++ InfNet/rocs_generation.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pickle
+from sklearn.metrics import roc_curve, auc
+
+roc_saves_dir = './roc_saves'
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+plt.figure()
+all_network_rocs = os.listdir(roc_saves_dir)
+
+for index, roc_file in enumerate(all_network_rocs):
+    roc_filename = os.path.join(roc_saves_dir, roc_file)
+
+    with open(roc_filename, 'rb') as f:
+        roc_dict = pickle.load(f)
+
+    roc_auc = auc(roc_dict['fpr'], roc_dict['tpr'])
+    lw = 2
+    plt.plot(roc_dict['fpr'], roc_dict['tpr'], color=colors[index],
+             lw=lw, label='ROC curve (area = %0.2f) %s' % (roc_auc, roc_file))
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.plot(roc_dict['optimal_fpr'], roc_dict['optimal_tpr'], 'go')
+    plt.annotate(f'{roc_dict["optimal_threshold"]}', (roc_dict['optimal_fpr'], roc_dict['optimal_tpr']))
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.legend(loc="lower right")
+
+plt.show()
