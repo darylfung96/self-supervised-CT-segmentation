e2a839593a6f90b386f1a73feff774db4c789e21
Daryl Fung <darylfung96@gmail.com>
2021-05-19 20:15:38 -0500

    fix

--- InfNet/combine_dataset.py
+++ InfNet/combine_dataset.py
@@ -8,30 +8,44 @@ set_name = ['Train', 'Test', 'Val']
 
 # LungInfection
 current_index = 0
-for current_type in lunginfection:
-    new_dir = f'Dataset/AllSet/LungInfection-All/{current_type}'
-    os.makedirs(new_dir, exist_ok=True)
-    for set_index in range(len(sets)):
-        current_dir = f'Dataset/{sets[set_index]}/LungInfection-{set_name[set_index]}/{current_type}'
-        all_items = natsorted(os.listdir(current_dir))
-        for item in all_items:
-            item_format = item.split('.')
-            item_format[0] = str(current_index)
-            new_name = '.'.join(item_format)
-            current_index += 1
-            copyfile(os.path.join(current_dir, item), os.path.join(new_dir, new_name))
+current_type = lunginfection[0]
+new_dir = f'Dataset/AllSet/LungInfection-All/{current_type}'
+os.makedirs(new_dir, exist_ok=True)
+for set_index in range(len(sets)):
+    current_dir = f'Dataset/{sets[set_index]}/LungInfection-{set_name[set_index]}'
+
+    training_data_dir = os.path.join(current_dir, current_type)
+    all_items = natsorted(os.listdir(training_data_dir))
+    for item in all_items:
+        item_format = item.split('.')
+        item_format[0] = str(current_index)
+        new_name = '.'.join(item_format)
+        current_index += 1
+        copyfile(os.path.join(training_data_dir, item), os.path.join(new_dir, new_name))
+
+        for other_type in lunginfection[1:]:
+            testing_data_dir = os.path.join(current_dir, other_type)
+            other_dir = f'Dataset/AllSet/LungInfection-All/{other_type}'
+            copyfile(os.path.join(testing_data_dir, item), os.path.join(other_dir, new_name))
 
 # MultiClassInfection
 current_index = 0
-for current_type in multiinfection:
-    new_dir = f'Dataset/AllSet/MultiClassInfection-All/{current_type}'
-    os.makedirs(new_dir, exist_ok=True)
-    for set_index in range(len(sets)):
-        current_dir = f'Dataset/{sets[set_index]}/MultiClassInfection-{set_name[set_index]}/{current_type}'
-        all_items = natsorted(os.listdir(current_dir))
-        for item in all_items:
-            item_format = item.split('.')
-            item_format[0] = str(current_index)
-            new_name = '.'.join(item_format)
-            current_index += 1
-            copyfile(os.path.join(current_dir, item), os.path.join(new_dir, new_name))
+current_type = lunginfection[0]
+new_dir = f'Dataset/AllSet/MultiClassInfection-All/{current_type}'
+os.makedirs(new_dir, exist_ok=True)
+for set_index in range(len(sets)):
+    current_dir = f'Dataset/{sets[set_index]}/MultiClassInfection-{set_name[set_index]}'
+
+    training_data_dir = os.path.join(current_dir, current_type)
+    all_items = natsorted(os.listdir(training_data_dir))
+    for item in all_items:
+        item_format = item.split('.')
+        item_format[0] = str(current_index)
+        new_name = '.'.join(item_format)
+        current_index += 1
+        copyfile(os.path.join(training_data_dir, item), os.path.join(new_dir, new_name))
+
+        for other_type in lunginfection[1:]:
+            testing_data_dir = os.path.join(current_dir, other_type)
+            other_dir = f'Dataset/AllSet/MultiClassInfection-All/{other_type}'
+            copyfile(os.path.join(testing_data_dir, item), os.path.join(other_dir, new_name))
