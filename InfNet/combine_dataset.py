3dbbb362d251fdbc8e5b8c7e18aae3d17748b040
Daryl Fung <darylfung96@gmail.com>
2021-05-19 20:35:48 -0500

    fix

--- InfNet/combine_dataset.py
+++ InfNet/combine_dataset.py
@@ -21,9 +21,8 @@ for set_index in range(len(sets)):
     training_data_dir = os.path.join(current_dir, current_type)
     all_items = natsorted(os.listdir(training_data_dir))
     for item in all_items:
-        item_format = item.split('.')
-        item_format[0] = str(current_index)
-        new_name = '.'.join(item_format)
+        item_name, item_format = item.split('.')
+        new_name = f'{str(current_index)}.{item_format}'
         current_index += 1
 
         # make sure the other files exist before copying (same filename)
@@ -32,7 +31,7 @@ for set_index in range(len(sets)):
             testing_data_dir = os.path.join(current_dir, other_type)
             other_item_format = os.listdir(testing_data_dir)[0].split('.')[1]
             other_dir = f'Dataset/AllSet/LungInfection-All/{other_type}'
-            if not os.path.exists(os.path.join(testing_data_dir, f'{str(current_index)}.{other_item_format}')):
+            if not os.path.exists(os.path.join(testing_data_dir, f'{item_name}.{other_item_format}')):
                 other_files_exist = False
                 continue
         if not other_files_exist:
@@ -43,7 +42,7 @@ for set_index in range(len(sets)):
             testing_data_dir = os.path.join(current_dir, other_type)
             other_item_format = os.listdir(testing_data_dir)[0].split('.')[1]
             other_dir = f'Dataset/AllSet/LungInfection-All/{other_type}'
-            copyfile(os.path.join(testing_data_dir, f'{str(current_index)}.{other_item_format}'),
+            copyfile(os.path.join(testing_data_dir, f'{item_name}.{other_item_format}'),
                      os.path.join(other_dir, f'{str(current_index)}.{other_item_format}'))
 
 # MultiClassInfection
@@ -61,9 +60,8 @@ for set_index in range(len(sets)):
     training_data_dir = os.path.join(current_dir, current_type)
     all_items = natsorted(os.listdir(training_data_dir))
     for item in all_items:
-        item_format = item.split('.')
-        item_format[0] = str(current_index)
-        new_name = '.'.join(item_format)
+        item_name, item_format = item.split('.')
+        new_name = f'{str(current_index)}.{item_format}'
         current_index += 1
 
         # make sure the other files exist before copying (same filename)
@@ -72,7 +70,7 @@ for set_index in range(len(sets)):
             testing_data_dir = os.path.join(current_dir, other_type)
             other_item_format = os.listdir(testing_data_dir)[0].split('.')[1]
             other_dir = f'Dataset/AllSet/MultiClassInfection-All/{other_type}'
-            if not os.path.exists(os.path.join(testing_data_dir, f'{str(current_index)}.{other_item_format}')):
+            if not os.path.exists(os.path.join(testing_data_dir, f'{item_name}.{other_item_format}')):
                 other_files_exist = False
                 continue
         if not other_files_exist:
@@ -81,5 +79,7 @@ for set_index in range(len(sets)):
         copyfile(os.path.join(training_data_dir, item), os.path.join(new_dir, new_name))
         for other_type in multiinfection[1:]:
             testing_data_dir = os.path.join(current_dir, other_type)
+            other_item_format = os.listdir(testing_data_dir)[0].split('.')[1]
             other_dir = f'Dataset/AllSet/MultiClassInfection-All/{other_type}'
-            copyfile(os.path.join(testing_data_dir, item), os.path.join(other_dir, new_name))
+            copyfile(os.path.join(testing_data_dir, f'{item_name}.{other_item_format}'),
+                     os.path.join(other_dir, f'{str(current_index)}.{other_item_format}'))
